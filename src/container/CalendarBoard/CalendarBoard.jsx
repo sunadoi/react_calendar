9318f6109ac17b28094ac42894af9d9246dd2ba5
sunadoi <y.sunadoi@gmail.com>
2020-05-13 19:39:57 +0900

    create calendar

--- src/container/CalendarBoard/CalendarBoard.jsx
+++ src/container/CalendarBoard/CalendarBoard.jsx
@@ -8,41 +8,84 @@ import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 
 const calendarBoard = props => {
-  const dayOfWeek = [ "日", "月", "火", "水", "木", "金", "土" ]
-  const dayOfWeekList = []
-  for (let i=0; i<dayOfWeek.length; i++) {
-    dayOfWeekList.push(<li className={classes.dayOfWeek}>{dayOfWeek[i]}</li>)
+  const weekDay = [ "日", "月", "火", "水", "木", "金", "土" ]
+  const weekDayList = []
+  weekDay.map((day, dayIndex) => {
+    weekDayList.push(<li className={classes.weekDay} key={dayIndex}>{day}</li>)
+  });
+
+
+  const today = new Date();
+  const firstDay = new Date();
+  const first = new Date(firstDay.setDate(1));
+  const getsumatsu = new Date(today.getFullYear(), today.getMonth(), 0);
+  const last = new Date(getsumatsu.setDate(1))
+  // console.log(today);
+  // console.log(copytoday);
+  // console.log(first);
+  // console.log(last);
+
+  const month = []
+
+  const createMonth = date => {
+    for (let week=0; week <= 5; week++) {
+      let day = new Date(new Date(date).setDate(date.getDate() + (7 * week)))
+
+      const lastDay = new Date(new Date(date.getFullYear(), date.getMonth()+1, 0))
+      month.push(createWeek(day))
+      if (week === 4 && lastDay.getDay() - day.getDay() >= 0) break; //最終日が第5週目だったらbreak
+    }
+    return month
   }
 
-  const weekList = []
-  for (let i=1; i<=7; i++) {
-    weekList.push(<CalendarElement />)
+  const createWeek = date => {
+    const baseDay = date.getDay();
+    const beforeBaseDay = [];
+    for (let beforeDay=0; beforeDay < baseDay; beforeDay++) {
+      let diffDayLength = baseDay - beforeDay
+      let diffDay = new Date(new Date(date).setDate(date.getDate() - diffDayLength));
+      beforeBaseDay.push(diffDay);
+    }
+
+    const afterBaseDay = [];
+    for (let afterDay=baseDay+1; afterDay <= 6; afterDay++) {
+      let diffDayLength = afterDay - baseDay
+      let diffDay = new Date(new Date(date).setDate(date.getDate() + diffDayLength));
+      afterBaseDay.push(diffDay);
+    }
+
+    return [...beforeBaseDay, date, ...afterBaseDay]
   }
 
-  const week = (
-    <div className={classes.Week}>
-      {weekList}
-    </div>
-  );
-
-  const weeksList = []
-  for (let i=1; i<=6; i++) {
-    weeksList.push(week)
+  const renderMonth = (month) => {
+    return (
+      <div className={classes.Month}>
+        {month.map((week, weekIndex) => {
+          return renderWeek(week, weekIndex)
+        })}
+      </div>
+    )
   }
 
-  const weeks = (
-    <div className={classes.Weeks}>
-      {weeksList}
-    </div>
-  )
+  const renderWeek = (week, weekIndex) => {
+    return (
+      <div className={classes.Week} key={weekIndex}>
+        {week.map((day, dayIndex) => {
+          return <CalendarElement date={day} key={dayIndex} />
+        })}
+      </div>
+    )
+  }
 
   const modal = props.showModal ? <AddSchedule  /> : null
+
+  createMonth(first);
 
   return (
     <div className={classes.CalendarBoard}>
       {modal}
-      <ul>{dayOfWeekList}</ul>
-      {weeks}
+      <ul>{weekDayList}</ul>
+      {renderMonth(month)}
     </div>
   )
 }
