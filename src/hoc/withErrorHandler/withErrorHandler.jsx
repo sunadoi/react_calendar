8a7e91d228fa2f4207baed874e350eb2a9dde21b
sunadoi <y.sunadoi@gmail.com>
2020-05-28 20:56:12 +0900

    create withErrorHandler

---
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent) => {
+  return props => {
+    return (
+      <>
+        <Modal>
+    <div style={{height: '100px', width: '400px', textAlign: 'center'}}><p style={{lineHeight: '100px'}}>{props.error}</p></div>
+        </Modal>
+        <WrappedComponent {...props} />
+      </>
+    )
+  }
+}
+
+export default withErrorHandler;
