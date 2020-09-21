52b0e5b77b035ef4af53a4cc41037e63c81d70a8
sunadoi <y.sunadoi@gmail.com>
2020-09-21 23:46:46 +0900

    スクロールバーの表示

--- src/components/UI/DatePicker/DatePicker.jsx
+++ src/components/UI/DatePicker/DatePicker.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import "date-fns";
 import DateFnsUtils from "@date-io/date-fns";
 import ja from "date-fns/locale/ja";
 import { ThemeProvider } from "@material-ui/core/styles";
