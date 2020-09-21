18894dcceb204c01d06f8031fe809ddc11bff547
sunadoi <y.sunadoi@gmail.com>
2020-09-21 09:45:03 +0900

    スケジュールの表記を変更

--- src/components/Navigation/Navbar/Navbar.jsx
+++ src/components/Navigation/Navbar/Navbar.jsx
@@ -77,7 +77,8 @@ const Navbar = (props) => {
               justifyContent="space-between"
               alignItems="center"
             >
-              <LabelIcon className={classes.AllIcon} />
+              <p style={{ marginTop: "0", marginRight: "4px" }}>🔵</p>
+              {/* <LabelIcon className={classes.AllIcon} /> */}
               <div>制限なし</div>
             </Box>
           </li>
@@ -87,7 +88,8 @@ const Navbar = (props) => {
               justifyContent="space-between"
               alignItems="center"
             >
-              <LabelIcon className={classes.PremiumIcon} />
+              <p style={{ marginTop: "0", marginRight: "4px" }}>🟢</p>
+              {/* <LabelIcon className={classes.PremiumIcon} /> */}
               <div>プレミアム以上限定</div>
             </Box>
           </li>
@@ -97,7 +99,8 @@ const Navbar = (props) => {
               justifyContent="space-between"
               alignItems="center"
             >
-              <LabelIcon className={classes.SuperPremiumIcon} />
+              <p style={{ marginTop: "0", marginRight: "4px" }}>🔴</p>
+              {/* <LabelIcon className={classes.SuperPremiumIcon} /> */}
               <div>スーパープレミアム以上限定</div>
             </Box>
           </li>
